564fecbc337dd72917eb8ff59af6bd4fbd35b4ab
Eric Evenchick <eric@evenchick.com>
2015-01-28 10:21:08 -0500

    proper VID, PID, and description

--- Src/usbd_desc.c
+++ Src/usbd_desc.c
@@ -56,14 +56,15 @@
 /** @defgroup USBD_DESC_Private_Defines
   * @{
   */
-#define USBD_VID     1155
-#define USBD_LANGID_STRING     1033
-#define USBD_MANUFACTURER_STRING     "STMicroelectronics"
-#define USBD_PID_FS     22336
-#define USBD_PRODUCT_STRING_FS     "CANtact"
+#define CANTACT_SW_VER			"v0.1"
+#define USBD_VID			0xad50
+#define USBD_LANGID_STRING		1033
+#define USBD_MANUFACTURER_STRING	"CANtact"
+#define USBD_PID_FS			0x60c4
+#define USBD_PRODUCT_STRING_FS		"CANtact" " " CANTACT_SW_VER
 #define USBD_SERIALNUMBER_STRING_FS     "00000000001A"
-#define USBD_CONFIGURATION_STRING_FS     "CDC Config"
-#define USBD_INTERFACE_STRING_FS     "CDC Interface"
+#define USBD_CONFIGURATION_STRING_FS    "CDC Config"
+#define USBD_INTERFACE_STRING_FS	"CDC Interface"
 
 /**
   * @}
