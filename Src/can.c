be7432a65f2adf2cbb26b9d2797e0455049f82a1
Ethan Zonca <ez@ethanzonca.com>
2016-08-11 21:52:05 -0400

    Formatting changes preparing for code overhaul

--- Src/can.c
+++ Src/can.c
@@ -7,7 +7,10 @@ CAN_FilterConfTypeDef filter;
 uint32_t prescaler;
 enum can_bus_state bus_state;
 
-void can_init(void) {
+
+// Initialize CAN peripheral settings, but don't actually start the peripheral
+void can_init(void)
+{
     filter.FilterIdHigh = 0;
     filter.FilterIdLow = 0;
     filter.FilterMaskIdHigh = 0;
@@ -25,19 +28,23 @@ void can_init(void) {
     bus_state = OFF_BUS;
 }
 
-void can_enable(void) {
-    if (bus_state == OFF_BUS) {
-	hcan.Init.Prescaler = prescaler;
-	hcan.Init.Mode = CAN_MODE_NORMAL;
-	hcan.Init.SJW = CAN_SJW_1TQ;
-	hcan.Init.BS1 = CAN_BS1_4TQ;
-	hcan.Init.BS2 = CAN_BS2_3TQ;
-	hcan.Init.TTCM = DISABLE;
-	hcan.Init.ABOM = DISABLE;
-	hcan.Init.AWUM = DISABLE;
-	hcan.Init.NART = DISABLE;
-	hcan.Init.RFLM = DISABLE;
-	hcan.Init.TXFP = DISABLE;
+
+// Start the CAN peripheral
+void can_enable(void)
+{
+    if (bus_state == OFF_BUS) 
+    {
+        hcan.Init.Prescaler = prescaler;
+        hcan.Init.Mode = CAN_MODE_NORMAL;
+        hcan.Init.SJW = CAN_SJW_1TQ;
+        hcan.Init.BS1 = CAN_BS1_4TQ;
+        hcan.Init.BS2 = CAN_BS2_3TQ;
+        hcan.Init.TTCM = DISABLE;
+        hcan.Init.ABOM = DISABLE;
+        hcan.Init.AWUM = DISABLE;
+        hcan.Init.NART = DISABLE;
+        hcan.Init.RFLM = DISABLE;
+        hcan.Init.TXFP = DISABLE;
         hcan.pTxMsg = NULL;
         HAL_CAN_Init(&hcan);
         HAL_CAN_ConfigFilter(&hcan, &filter);
@@ -45,8 +52,12 @@ void can_enable(void) {
     }
 }
 
-void can_disable(void) {
-    if (bus_state == ON_BUS) {
+
+// Disable the CAN peripheral and go off-bus
+void can_disable(void)
+{
+    if (bus_state == ON_BUS)
+    {
         // do a bxCAN reset (set RESET bit to 1)
         hcan.Instance->MCR |= CAN_MCR_RESET;
         bus_state = OFF_BUS;
@@ -54,56 +65,69 @@ void can_disable(void) {
     HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_RESET);
 }
 
-void can_set_bitrate(enum can_bitrate bitrate) {
-    if (bus_state == ON_BUS) {
+
+// Set the bitrate of the CAN peripheral
+void can_set_bitrate(enum can_bitrate bitrate)
+{
+    if (bus_state == ON_BUS)
+    {
         // cannot set bitrate while on bus
         return;
     }
 
-    switch (bitrate) {
-    case CAN_BITRATE_10K:
-	prescaler = 600;
-        break;
-    case CAN_BITRATE_20K:
-	prescaler = 300;
-        break;
-    case CAN_BITRATE_50K:
-	prescaler = 120;
-        break;
-    case CAN_BITRATE_100K:
-        prescaler = 60;
-        break;
-    case CAN_BITRATE_125K:
-        prescaler = 48;
-        break;
-    case CAN_BITRATE_250K:
-        prescaler = 24;
-        break;
-    case CAN_BITRATE_500K:
-        prescaler = 12;
-        break;
-    case CAN_BITRATE_750K:
-        prescaler = 8;
-        break;
-    case CAN_BITRATE_1000K:
-        prescaler = 6;
-        break;
+    switch (bitrate)
+    {
+        case CAN_BITRATE_10K:
+        prescaler = 600;
+            break;
+        case CAN_BITRATE_20K:
+        prescaler = 300;
+            break;
+        case CAN_BITRATE_50K:
+        prescaler = 120;
+            break;
+        case CAN_BITRATE_100K:
+            prescaler = 60;
+            break;
+        case CAN_BITRATE_125K:
+            prescaler = 48;
+            break;
+        case CAN_BITRATE_250K:
+            prescaler = 24;
+            break;
+        case CAN_BITRATE_500K:
+            prescaler = 12;
+            break;
+        case CAN_BITRATE_750K:
+            prescaler = 8;
+            break;
+        case CAN_BITRATE_1000K:
+            prescaler = 6;
+            break;
     }
 }
 
-void can_set_silent(uint8_t silent) {
-    if (bus_state == ON_BUS) {
+
+// Set CAN peripheral to silent mode
+void can_set_silent(uint8_t silent)
+{
+    if (bus_state == ON_BUS)
+    {
         // cannot set silent mode while on bus
         return;
     }
-    if (silent) {
+    if (silent)
+    {
         hcan.Init.Mode = CAN_MODE_SILENT;
     } else {
         hcan.Init.Mode = CAN_MODE_NORMAL;
     }
 }
 
-uint32_t can_tx(CanTxMsgTypeDef *tx_msg, uint32_t timeout) {
+
+// Send a message on the CAN bus (blocking)
+uint32_t can_tx(CanTxMsgTypeDef *tx_msg, uint32_t timeout)
+{
     uint32_t status;
 
     // transmit can frame
@@ -114,7 +138,10 @@ uint32_t can_tx(CanTxMsgTypeDef *tx_msg, uint32_t timeout) {
     return status;
 }
 
-uint32_t can_rx(CanRxMsgTypeDef *rx_msg, uint32_t timeout) {
+
+// Receive a message from the CAN bus (blocking)
+uint32_t can_rx(CanRxMsgTypeDef *rx_msg, uint32_t timeout)
+{
     uint32_t status;
 
     hcan.pRxMsg = rx_msg;
@@ -125,8 +152,12 @@ uint32_t can_rx(CanRxMsgTypeDef *rx_msg, uint32_t timeout) {
     return status;
 }
 
-uint8_t is_can_msg_pending(uint8_t fifo) {
-    if (bus_state == OFF_BUS) {
+
+// Check if a CAN message has been received and is waiting in the FIFO
+uint8_t is_can_msg_pending(uint8_t fifo)
+{
+    if (bus_state == OFF_BUS)
+    {
         return 0;
     }
     return (__HAL_CAN_MSG_PENDING(&hcan, fifo) > 0);
--- Src/slcan.c
+++ Src/slcan.c
@@ -1,18 +1,24 @@
+//
+// slcan: Parse incoming and generate outgoing slcan messages
+
 #include "stm32f0xx_hal.h"
 #include "can.h"
 #include "slcan.h"
 
-int8_t slcan_parse_frame(uint8_t *buf, CanRxMsgTypeDef *frame) {
+int8_t slcan_parse_frame(uint8_t *buf, CanRxMsgTypeDef *frame)
+{
     uint8_t i = 0;
     uint8_t id_len, j;
     uint32_t tmp;
 
-    for (j=0; j < SLCAN_MTU; j++) {
+    for (j=0; j < SLCAN_MTU; j++)
+    {
         buf[j] = '\0';
     }
 
     // add character for frame type
-    if (frame->RTR == CAN_RTR_DATA) {
+    if (frame->RTR == CAN_RTR_DATA)
+    {
         buf[i] = 't';
     } else if (frame->RTR == CAN_RTR_REMOTE) {
         buf[i] = 'r';
@@ -21,8 +27,10 @@ int8_t slcan_parse_frame(uint8_t *buf, CanRxMsgTypeDef *frame) {
     // assume standard identifier
     id_len = SLCAN_STD_ID_LEN;
     tmp = frame->StdId;
+
     // check if extended
-    if (frame->IDE == CAN_ID_EXT) {
+    if (frame->IDE == CAN_ID_EXT)
+    {
         // convert first char to upper case for extended frame
         buf[i] -= 32;
         id_len = SLCAN_EXT_ID_LEN;
@@ -31,7 +39,8 @@ int8_t slcan_parse_frame(uint8_t *buf, CanRxMsgTypeDef *frame) {
     i++;
 
     // add identifier to buffer
-    for(j=id_len; j > 0; j--) {
+    for(j=id_len; j > 0; j--)
+    {
         // add nybble to buffer
         buf[j] = (tmp & 0xF);
         tmp = tmp >> 4;
@@ -42,13 +51,15 @@ int8_t slcan_parse_frame(uint8_t *buf, CanRxMsgTypeDef *frame) {
     buf[i++] = frame->DLC;
 
     // add data bytes
-    for (j = 0; j < frame->DLC; j++) {
+    for (j = 0; j < frame->DLC; j++)
+    {
         buf[i++] = (frame->Data[j] >> 4);
         buf[i++] = (frame->Data[j] & 0x0F);
     }
 
     // convert to ASCII (2nd character to end)
-    for (j = 1; j < i; j++) {
+    for (j = 1; j < i; j++)
+    {
         if (buf[j] < 0xA) {
             buf[j] += 0x30;
         } else {
@@ -63,12 +74,15 @@ int8_t slcan_parse_frame(uint8_t *buf, CanRxMsgTypeDef *frame) {
     return i;
 }
 
-int8_t slcan_parse_str(uint8_t *buf, uint8_t len) {
+
+int8_t slcan_parse_str(uint8_t *buf, uint8_t len)
+{
     CanTxMsgTypeDef frame;
     uint8_t i;
 
     // convert from ASCII (2nd character to end)
-    for (i = 1; i < len; i++) {
+    for (i = 1; i < len; i++)
+    {
         // lowercase letters
         if(buf[i] >= 'a')
             buf[i] = buf[i] - 'a' + 10;
@@ -80,7 +94,8 @@ int8_t slcan_parse_str(uint8_t *buf, uint8_t len) {
             buf[i] = buf[i] - '0';
     }
 
-    if (buf[0] == 'O') {
+    if (buf[0] == 'O')
+    {
         // open channel command
         can_enable();
         return 0;
@@ -92,43 +107,45 @@ int8_t slcan_parse_str(uint8_t *buf, uint8_t len) {
 
     } else if (buf[0] == 'S') {
         // set bitrate command
-        switch(buf[1]) {
-        case 0:
-            can_set_bitrate(CAN_BITRATE_10K);
-            break;
-        case 1:
-            can_set_bitrate(CAN_BITRATE_20K);
-            break;
-        case 2:
-            can_set_bitrate(CAN_BITRATE_50K);
-            break;
-        case 3:
-            can_set_bitrate(CAN_BITRATE_100K);
-            break;
-        case 4:
-            can_set_bitrate(CAN_BITRATE_125K);
-            break;
-        case 5:
-            can_set_bitrate(CAN_BITRATE_250K);
-            break;
-        case 6:
-            can_set_bitrate(CAN_BITRATE_500K);
-            break;
-        case 7:
-            can_set_bitrate(CAN_BITRATE_750K);
-            break;
-        case 8:
-            can_set_bitrate(CAN_BITRATE_1000K);
-            break;
-        default:
-            // invalid setting
-            return -1;
+        switch(buf[1])
+        {
+            case 0:
+                can_set_bitrate(CAN_BITRATE_10K);
+                break;
+            case 1:
+                can_set_bitrate(CAN_BITRATE_20K);
+                break;
+            case 2:
+                can_set_bitrate(CAN_BITRATE_50K);
+                break;
+            case 3:
+                can_set_bitrate(CAN_BITRATE_100K);
+                break;
+            case 4:
+                can_set_bitrate(CAN_BITRATE_125K);
+                break;
+            case 5:
+                can_set_bitrate(CAN_BITRATE_250K);
+                break;
+            case 6:
+                can_set_bitrate(CAN_BITRATE_500K);
+                break;
+            case 7:
+                can_set_bitrate(CAN_BITRATE_750K);
+                break;
+            case 8:
+                can_set_bitrate(CAN_BITRATE_1000K);
+                break;
+            default:
+                // invalid setting
+                return -1;
         }
         return 0;
 
     } else if (buf[0] == 'm' || buf[0] == 'M') {
         // set mode command
-        if (buf[1] == 1) {
+        if (buf[1] == 1) 
+        {
             // mode 1: silent
             can_set_silent(1);
         } else {
